e3097230c085daba73ff338c8317c260801a8aad
root <root@ip-172-31-92-186.ec2.internal>
2024-02-19 13:51:46 +0000

    modified

--- simple.sh
+++ simple.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 echo "Hello World"
 echo "guru biswagar"
-##############################################
 echo "devops is my fev tool"
 echo "and i wiil be a great devops engineer"
+
